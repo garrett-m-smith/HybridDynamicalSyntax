11676237434fe6088bd8c37b9e67d46b3c674d5a
Garrett Smith <garrett.smith@uconn.edu>
2015-09-01 13:44:34 -0400

    Added notes for future testing

--- Learn11a/events.m
+++ Learn11a/events.m
@@ -4,6 +4,9 @@ function [value,isterminal,direction] = events11(tt, zz, sys)
 
 crit = 0.05;
 value = [zz(3)*(1- zz(3)) - crit, zz(5) - 39];
+% value = [zz(3)*(1- zz(3)) - crit, zz(5) - 39]; % change to input -
+% state^2; remember the deriv of this fn. in other scripts!
+% then play with to make input = 1 ~> approach fp. @ 0, z.B.
 % value = [zz(3) - crit, zz(5) - 39];
 isterminal = [1, 1]; % event should terminate when value(1) or tt - 1 = 0
-direction = [0, 1]; % says value fn. should just = 0
+direction = [-1, 1]; % says value fn. should just = 0
